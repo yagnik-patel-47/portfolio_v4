ff7f249d05fedec3bda9e0436d320fd36d548263
Yagnik Patel <coderisaddicted@gmail.com>
2023-10-20 21:10:52 +0530

    project page added

--- src/lib/projects-data.ts
+++ src/lib/projects-data.ts
@@ -1,6 +1,10 @@
 import HigrowImage from "@/assets/projects/higrow.webp";
 import RPSImage from "@/assets/projects/rps.webp";
 import JpDisplayImage from "@/assets/projects/jp.webp";
+import TodoImage from "@/assets/projects/todo.webp"
+import GalaryImage from "@/assets/projects/galary.webp"
+import LegoImage from "@/assets/projects/lego.webp"
+import SMAImage from "@/assets/projects/sma.webp"
 
 export const projects = [
   {
@@ -12,7 +16,7 @@ export const projects = [
     links: {
       site: "https://higrow.xyz",
     },
-    category: "real-life",
+    category: "real-world",
   },
   {
     name: "PvP Rock Paper Scissors",
@@ -30,13 +34,61 @@ export const projects = [
     name: "Japanese Art",
     discription: "A static page website with awesome animations.",
     image: JpDisplayImage,
-    tech: ["Astro", "GSAP"],
+    tech: ["Astro", "GSAP", "TailWind CSS"],
     links: {
       site: "https://jp-display.vercel.app",
       repo: "https://github.com/yagnik-patel-47/jp_display",
     },
     category: "static",
   },
+  {
+    name: "Lego One",
+    discription: "A landing page for real estate ecommerce website with somewhat good design.",
+    image: LegoImage,
+    tech: ["Nextjs", "TailWind CSS"],
+    links: {
+      site: "https://lego-one.vercel.app",
+      repo: "https://github.com/yagnik-patel-47/lego",
+    },
+    category: "static",
+  },
+  {
+    name: "Animation Galary",
+    discription: "A single page image gallery site with awesome animations.",
+    image: GalaryImage,
+    tech: ["React", "Framer Motion"],
+    links: {
+      site: "https://animated0casestudy.vercel.app",
+      repo: "https://github.com/yagnik-patel-47/animated_casestudy",
+    },
+    category: "static",
+  },
+  {
+    name: "Todo Web App",
+    discription: "A Todo App made using vanilla javascript with animejs animations.",
+    image: TodoImage,
+    tech: ["Javascript", "Anime.js"],
+    links: {
+      site: "https://todoapp-ca.netlify.app",
+      repo: "https://github.com/Yagnik-Patel-47/vanilla-js-to-do",
+    },
+    category: "app",
+  },
+  {
+    name: "Social Media App",
+    discription: "A social media app clone having basic features similar to Instagram.",
+    image: SMAImage,
+    tech: ["Nextjs", "Firebase", "Typescript", "Redux"],
+    links: {
+      site: "https://moments-sma.vercel.app",
+      repo: "https://github.com/Yagnik-Patel-47/social-media-app",
+    },
+    category: "fullstack",
+  },
 ];
 
 export const featuredProjects = [projects[0], projects[1]];
+export const realWorldProjects = projects.filter(project => project.category === "real-world");
+export const fullstackProjects = projects.filter(project => project.category === "fullstack");
+export const staticProjects = projects.filter(project => project.category === "static");
+export const appProjects = projects.filter(project => project.category === "app");
