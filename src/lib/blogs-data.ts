32b23c181e9e1c920e8829ea2d21f6a7dc6534a4
Yagnik Patel <coderisaddicted@gmail.com>
2023-12-19 23:24:26 +0530

    depenencies update and code refactore

--- src/lib/blogs-data.ts
+++ src/lib/blogs-data.ts
@@ -2,7 +2,7 @@ import SupabaseVsFirebaseImage from "@/assets/blog/svf.webp";
 
 export const blogs = [
   {
-    title: "Supabase vs Firebase",
+    title: "Firebase vs Supabase",
     date: "Aug 14, 2023",
     link: "https://mytakeonweb.hashnode.dev/firebase-vs-supabase",
     image: SupabaseVsFirebaseImage,
