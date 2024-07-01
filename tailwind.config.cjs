224b4140509ade3e9e87463664374138209a01c8
Yagnik Patel <coderisaddicted@gmail.com>
2024-07-01 12:23:23 +0530

    navbar ui rework

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
-        serif: ["Bricolage Grotesque Variable", ...defaultTheme.fontFamily.serif],
-      },
-    },
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	darkMode: "class",
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
+				serif: [
+					"Bricolage Grotesque Variable",
+					...defaultTheme.fontFamily.serif,
+				],
+			},
+		},
+	},
+	plugins: [],
+};
