bbf9e7df714bb84a6020a7c43616fca121b72615
Yagnik Patel <coderisaddicted@gmail.com>
2024-11-02 13:57:12 +0530

    refactor: directory routes to files & slider improvements

--- src/lib/icons-map.ts
+++ src/lib/icons-map.ts
@@ -1,17 +1,18 @@
 import NextjsIcon from "@/assets/icons/skill/nextjs.svg";
 import SvelteIcon from "@/assets/icons/skill/svelte.svg";
-import DrizzleIcon from "@/assets/icons/skill/drizzle.svg";
-import StripeIcon from "@/assets/icons/skill/stripe.svg";
+import DrizzleIcon from "@/assets/icons/skill/drizzle.webp";
+import StripeIcon from "@/assets/icons/skill/stripe.webp";
 import SupabaseIcon from "@/assets/icons/skill/supabase.svg";
 import DenoIcon from "@/assets/icons/skill/deno.svg";
 import SocketIOIcon from "@/assets/icons/skill/socketio.svg";
 import ReactIcon from "@/assets/icons/skill/react.svg";
 import AstroIcon from "@/assets/icons/skill/astro.svg";
-import GSAPIcon from "@/assets/icons/skill/gsap.svg";
+import GSAPIcon from "@/assets/icons/skill/gsap.webp";
 import TailwindCSSIcon from "@/assets/icons/skill/tailwindcss.svg";
 import FirebaseIcon from "@/assets/icons/skill/firebase.svg";
 import TypescriptIcon from "@/assets/icons/skill/typescript.svg";
 import ReduxIcon from "@/assets/icons/skill/redux.svg";
+import FramerIcon from "@/assets/icons/skill/framer.svg";
 
 export const skillIcons = {
 	Nextjs: NextjsIcon,
@@ -28,4 +29,5 @@ export const skillIcons = {
 	Firebase: FirebaseIcon,
 	Typescript: TypescriptIcon,
 	Redux: ReduxIcon,
+	"Framer Motion": FramerIcon,
 };
