5371e2e448f5ab7c9a8c553593ce87dfb538a348
Yagnik Patel <coderisaddicted@gmail.com>
2024-12-06 01:21:20 +0530

    vercel build fail fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,11 @@ export default defineConfig({
 	],
 	vite: {
 		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				"@": "/src",
+			},
+		},
 		css: {
 			transformer: "lightningcss",
 		},
